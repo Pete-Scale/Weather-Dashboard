2f88d5415f6583ce68eb14ecb438866283bafd86
Pete-Scale <petescale@gmail.com>
2020-12-17 15:43:05 -0600

    got temperature through loop

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,7 @@ function getWeather(searchCity) {
         // city name
         console.log(response)
         cityName.text(response.name)
-        var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?appid=7eef78754aaa2ca60da79139ada4f6f4&lat=' + lat + '&lon=' + lon;
+        var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?units=imperial&appid=7eef78754aaa2ca60da79139ada4f6f4&lat=' + lat + '&lon=' + lon;
         // Uses coordinates from weather ajax call
         $.ajax({
             url: queryURL,
@@ -37,7 +37,7 @@ function getWeather(searchCity) {
             console.log(response)
             // Loop through daily array
             for (var i = 0; i < response.daily.length - 2; i++) {
-                // Get time and format to XX/XX/XXXX
+                // Get time from unix time stamp and format to XX/XX/XXXX
                 var unixTimeStamp = response.daily[i].dt
                 var date = new Date(unixTimeStamp * 1000);
                 var month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -51,6 +51,9 @@ function getWeather(searchCity) {
                 var iconID = response.daily[i].weather[0].icon
                 $('#weather-img-' + i).attr('src', 'http://openweathermap.org/img/wn/' + iconID + '@2x.png');
                 
+                // Get temperature
+                var tempID = Math.floor(response.daily[i].temp.max);
+                $('#temp-' + i).text(tempID + ' °F')
             }
         })
     }); 
