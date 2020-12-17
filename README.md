1bed4522282c96881e0d5aa1026b9f0452b697eb
Pete-Scale <petescale@gmail.com>
2020-12-17 14:50:50 -0600

    looped through daily array to get each day time

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Weather-Dashboard
 
 ## Credits
+
+https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+
 https://stackoverflow.com/questions/9474752/javascript-get-month-year-day-from-unix-timestamp
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
 var cityName = $('#city-name')
-
-var apiKey = '7eef78754aaa2ca60da79139ada4f6f4';
 var searchCity;
 var queryURL_getCoords = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=7eef78754aaa2ca60da79139ada4f6f4&q=';
 // Click listeners
 $('#search-button').on('click', search)
 
+// Search city when search button is clicked 
 function search(event) {
     event.preventDefault();
     cityInput = $('#search-input').val().trim();
@@ -15,10 +14,11 @@ function search(event) {
     }
 }
 
-// Gets coordinates by city name so we can make the onecall ajax call 
+// Gets weather data when search button is clicked
 function getWeather(searchCity) {
     var lat;
     var lon;
+    // Gets coordinates by city name so we can make the onecall ajax call 
     $.ajax({
         url: queryURL_getCoords + searchCity,
         method: 'GET'
@@ -29,17 +29,25 @@ function getWeather(searchCity) {
         console.log(response)
         cityName.text(response.name)
         var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?appid=7eef78754aaa2ca60da79139ada4f6f4&lat=' + lat + '&lon=' + lon;
+        // Uses coordinates from weather ajax call
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function(response){
             console.log(response)
-            unixTimeStamp = response.current.dt
-            var date = new Date(unixTimeStamp * 1000);
-            var month = (date.getMonth() + 1).toString().padStart(2, '0');
-            var day = date.getDate().toString().padStart(2, '0');
-            var year = date.getFullYear();
-            console.log(month + '/' + day + '/' + year);
+            // Loop through daily array
+            for (var i = 0; i < response.daily.length - 2; i++) {
+                // Get time and format to XX/XX/XXXX
+                var unixTimeStamp = response.daily[i].dt
+                var date = new Date(unixTimeStamp * 1000);
+                var month = (date.getMonth() + 1).toString().padStart(2, '0');
+                var day = date.getDate().toString().padStart(2, '0');
+                var year = date.getFullYear();
+                console.log(month + '/' + day + '/' + year);
+                $('#date-' + i).text('(' + month + '/' + day + '/' + year + ')');
+                
+                // $('#weather-img-0').attr('src', 'https://openweather.org/img/w/' + )
+            }
         })
     }); 
 }
