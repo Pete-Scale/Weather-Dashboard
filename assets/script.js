72609d61a2b0d05b1c6d641c9d0e870fcf998779
Pete-Scale <petescale@gmail.com>
2020-12-17 12:29:40 -0600

    function to get coordinates by city name and put into onecall

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,29 @@
-var apiKey = '7eef78754aaa2ca60da79139ada4f6f4'
-var cityName = 'Chicago'
+var apiKey = '7eef78754aaa2ca60da79139ada4f6f4';
+var cityName = 'Chicago';
 
-'api.openweathermap.org/data/2.5/forecast?q='+ cityName + '&appid=' + apiKey
+var queryURL_getCoords = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=7eef78754aaa2ca60da79139ada4f6f4&q=' + cityName;
+
+
+// Gets coordinates by city name so we can make the onecall ajax call 
+function getWeather() {
+    var lat;
+    var lon;
+    $.ajax({
+        url: queryURL_getCoords,
+        method: 'GET'
+    }).then(function(response) {
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?appid=7eef78754aaa2ca60da79139ada4f6f4&lat=' + lat + '&lon=' + lon;
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response){
+            console.log(response)
+        })
+    }); 
+}
+
+getWeather();
+
+
