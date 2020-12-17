b30c1e07177671d759b6a4a44bd33fd6384d8d09
Pete-Scale <petescale@gmail.com>
2020-12-17 14:16:31 -0600

    got unix date converted and credit to README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Weather-Dashboard
+
+## Credits
+https://stackoverflow.com/questions/9474752/javascript-get-month-year-day-from-unix-timestamp
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
+var cityName = $('#city-name')
+
 var apiKey = '7eef78754aaa2ca60da79139ada4f6f4';
-var cityName;
+var searchCity;
 var queryURL_getCoords = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=7eef78754aaa2ca60da79139ada4f6f4&q=';
 // Click listeners
 $('#search-button').on('click', search)
@@ -8,27 +10,36 @@ function search(event) {
     event.preventDefault();
     cityInput = $('#search-input').val().trim();
     if (cityInput !== '') {
-        cityName = cityInput;
-        getWeather(cityName);
+        searchCity = cityInput;
+        getWeather(searchCity);
     }
 }
 
 // Gets coordinates by city name so we can make the onecall ajax call 
-function getWeather(cityName) {
+function getWeather(searchCity) {
     var lat;
     var lon;
     $.ajax({
-        url: queryURL_getCoords + cityName,
+        url: queryURL_getCoords + searchCity,
         method: 'GET'
     }).then(function(response) {
         lat = response.coord.lat;
         lon = response.coord.lon;
+        // city name
+        console.log(response)
+        cityName.text(response.name)
         var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?appid=7eef78754aaa2ca60da79139ada4f6f4&lat=' + lat + '&lon=' + lon;
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function(response){
             console.log(response)
+            unixTimeStamp = response.current.dt
+            var date = new Date(unixTimeStamp * 1000);
+            var month = (date.getMonth() + 1).toString().padStart(2, '0');
+            var day = date.getDate().toString().padStart(2, '0');
+            var year = date.getFullYear();
+            console.log(month + '/' + day + '/' + year);
         })
     }); 
 }
